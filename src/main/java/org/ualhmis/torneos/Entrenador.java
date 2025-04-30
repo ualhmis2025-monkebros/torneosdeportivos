da134c767d1c2e9c151053e8cb42e36d15b20e48
MANUEL QUINTA SÁNCHEZ <mqs562@inlumine.ual.es>
2025-04-30 18:58:15 +0200

    Prueba 1

--- src/main/java/org/ualhmis/torneos/Entrenador.java
+++ src/main/java/org/ualhmis/torneos/Entrenador.java
@@ -1,7 +1,7 @@
 package org.ualhmis.torneos;
 
 import java.time.LocalDate;
-
+//
 class Entrenador extends Persona {
     public Entrenador(String nombre, String genero, LocalDate fechaNacimiento) {
         super(nombre, genero, fechaNacimiento);
